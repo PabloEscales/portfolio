846dcfbbb6414a366272c5bd54860612657b63d7
Pablo Escales <pabloescales@gmail.com>
2022-11-10 09:49:33 +0100

    BackgroundCircles Component added

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import BackgroundCircles from '../components/BackgroundCircles';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import { motion } from 'framer-motion';
 
 type Props = {}
 
@@ -8,16 +10,20 @@ export default function Hero({}: Props) {
       words: [
         `Hi, I'm Pablo Escales`,
         `creativy-driven-developer.tsx`,
-        `<who-enjoy-coding />`
+        `<who-enjoy-the way of new coding />`
       ],
     loop: true,
     delaySpeed: 2000,
     });
 
   return (
-    <div>
-      <span>{text}</span>
-      <Cursor cursorColor='#F7AB0A' />
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      <BackgroundCircles />
+      <h2 className='text-xl font-burtons dark:text-gray-500'>Jr. Frontend Developer</h2>
+      <h1>
+        <span className='text-xl font-burtons dark:text-gray-400'>{text}</span>
+        <Cursor cursorColor='#F7AB0A' />
+      </h1>
     </div>
   )
 }
