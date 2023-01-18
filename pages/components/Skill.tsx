582c4d8cbd018b9228d4fbab709c3865095c6e1d
Pablo Escales <pabloescales@gmail.com>
2023-01-18 18:05:04 +0100

    newtestingdeploy

--- pages/components/Skill.tsx
+++ pages/components/Skill.tsx
@@ -5,10 +5,10 @@ type Props = {
   directionalLeft?: boolean;
 };
 
-function Skill(Props) {
+function Skill({ directionaLeft }: Props) {
   return (
     <div className='group relative flex cursor-pointer'>
-      {/* <motion.img
+      <motion.img
         initial={{
           x: directionaLeft ? -600 : 600,
           opacity: 0,
@@ -17,7 +17,7 @@ function Skill(Props) {
         whileInView={{ opacity: 1, x: 0 }}
         src='https://raw.githubusercontent.com/devicons/devicon/master/icons/rails/rails-original-wordmark.svg'
         className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
-      /> */}
+      />
       <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
           <p className='text-3xl font-bold text-black opacity-100'>
