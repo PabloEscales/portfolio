81d15464e5d2720be945f8cc7de19c287bc4eb33
Pablo Escales <pabloescales@gmail.com>
2023-04-27 09:51:48 +0200

    CV updated

--- pages/index.jsx
+++ pages/index.jsx
@@ -90,10 +90,10 @@ export default function Home() {
                 <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer dark:text-gray-400'/></li>
                 <li>
                   <a className='bg-yellow-500/80 text-black px-3 flex py-2 rounded-md ml-8 dark:text-gray-800' href='#' download='Resume Pablo Escales Frontend Web Developer' onClick={() => {downloadResume(PDF_RESUME)}} target="_blank">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                   </svg>
-                    &nbsp; Resume
+                    &nbsp; CV
                   </a>
                 </li>
                 <li>
